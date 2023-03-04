6d2f802ae57ecce81d6f6222841d37835d4e016f
Towkir Ahmed <towkir17@gmail.com>
2023-03-04 13:37:52 +0600

    added basic markup for word input

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div id="app">
     <Header/>
+    <word-input />
   </div>
 </template>
 
 <script>
 import Header from '@/components/Header.vue'
+import WordInput from '@/components/WordInput.vue'
 
 export default {
   name: 'App',
   components: {
     Header,
+    WordInput,
   }
 }
 </script>
---
+++ src/components/WordInput.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="input-wrapper">
+    <input type="text">
+    <span class="search">
+      <img src="@/assets/images/icon-search.svg" alt="search-icon">
+    </span>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
