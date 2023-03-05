84610f92d72d60b9827c6acd0d7bab29c04b5d05
Towkir Ahmed <towkir17@gmail.com>
2023-03-05 21:49:21 +0600

    added a few color variants

--- src/App.vue
+++ src/App.vue
@@ -26,6 +26,17 @@ export default {
   font-size: 14px;
   line-height: 17px;
   font-weight: 400;
+  --purple: #A445ED;
+  --red: #FF5252;
+  --black-variant-1: #050505;
+  --black-variant-2: #1F1F1F;
+  --black-variant-3: #2D2D2D;
+  --black-variant-4: #3A3A3A;
+  --grey-variant-1: #757575;
+  --grey-variant-2: #E9E9E9;
+  --grey-variant-3: #F4F4F4;
+  --white: #FFFFFF;
+  --shadow-color: #0000001A;
 }
 
 :root[font-family="serif"] {
