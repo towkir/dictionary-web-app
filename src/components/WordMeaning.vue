9d47f0aebbf6107c1656cd32ce065801572eb28a
Towkir Ahmed <towkir17@gmail.com>
2023-03-08 01:20:03 +0600

    added component for word meaning

---
+++ src/components/WordMeaning.vue
@@ -0,0 +1,105 @@
+<template>
+  <div v-if="meaning" class="word-meaning">
+    <div class="part-of-speech">
+      <h3>{{meaning.partOfSpeech}}</h3>
+      <span class="border"></span>
+    </div>
+    <h5>Meaning</h5>
+    <ul class="meaning-list">
+      <li
+        v-for="(item, i) in meaning.definitions"
+        :key="`definition-${i}`"
+      >
+        {{ item.definition }}
+        <span v-if="item.example" class="example">
+          "{{ item.example }}"
+        </span>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'WordMeaning',
+  props: {
+    meaning: {
+      type: Object,
+    }
+  }
+}
+</script>
+
+<style>
+.word-meaning {
+  margin-bottom: 40px;
+}
+
+.word-meaning .part-of-speech {
+  display: flex;
+  margin-bottom: 40px;
+}
+
+.word-meaning .part-of-speech h3 {
+  font-size: 1.68rem;
+  line-height: 2.1rem;
+  font-weight: 700;
+  font-style: italic;
+}
+
+.word-meaning .part-of-speech span {
+  display: inline-block;
+  flex-grow: 1;
+  position: relative;
+}
+
+.word-meaning .part-of-speech span:after {
+  content: '';
+  position: absolute;
+  height: 1px;
+  width: calc(100% - 20px);
+  top: 50%;
+  right: 0;
+  background-color: var(--hr-color);
+  transition: 0.3s ease-in-out;
+}
+
+.word-meaning h5 {
+  font-size: 1.4rem;
+  line-height: 1.68rem;
+  color: var(--grey-variant-1);
+  font-weight: 400;
+  margin-bottom: 24px;
+}
+
+.word-meaning ul.meaning-list {
+  margin-left: 22px;
+  list-style: none;
+}
+
+.word-meaning ul.meaning-list li {
+  font-size: 1.26rem;
+  line-height: 1.68rem;
+  margin-bottom: 12px;
+  padding-left: 25px;
+  position: relative;
+}
+
+.word-meaning ul.meaning-list li:before {
+  content: '';
+  display: inline-block;
+  width: 5px;
+  height: 5px;
+  border-radius: 5px;
+  background-color: var(--purple);
+  position: absolute;
+  left: 0;
+  top: 10px;
+}
+
+.word-meaning ul.meaning-list li span.example {
+  display: block;
+  color: var(--grey-variant-1);
+  margin-top: 12px;
+}
+</style>
