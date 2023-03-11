b78eec88b7b1fda73339b9be34b6d1216bd8a215
Towkir Ahmed <towkir17@gmail.com>
2023-03-11 21:22:56 +0600

    replaced svg file with inline svg code

--- src/components/FontSelector.vue
+++ src/components/FontSelector.vue
@@ -4,8 +4,7 @@
       class="selected-font"
       @click="toggleFontPicker"
     >
-      {{ selectedFont.label }}
-      <img src="@/assets/images/icon-arrow-down.svg" class="icon-dropdown"/> 
+      {{ selectedFont.label }}<svg class="icon-dropdown" xmlns="http://www.w3.org/2000/svg" width="14" height="8" viewBox="0 0 14 8"><path fill="none" stroke="#A445ED" stroke-width="1.5" d="m1 1 6 6 6-6"/></svg>
     </div>
     <div
       v-if="fontPickerOpen"
